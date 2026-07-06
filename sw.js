82499c84f50e26ceae6b7f00c61eecc7d5c0ed65
Rafieiy <davieiycode@users.noreply.github.com>
2026-07-07 02:48:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,6 @@
-self.addEventListener('install', (e) => { self.skipWaiting(); });
-self.addEventListener('fetch', (e) => {});
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+self.addEventListener('fetch', (e) => {
+  // Hanya bypass untuk verifikasi install Chrome Android
+});
