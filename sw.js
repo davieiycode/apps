99051f1f28d4133b61fb419546096d38475eab5d
davieiycode <rafieiy.m@gmail.com>
2026-07-07 04:52:19 +0700

    Fix manifest scope and sw fetch handler for PWA standalone

--- sw.js
+++ sw.js
@@ -2,5 +2,7 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 self.addEventListener('fetch', (e) => {
-  // Hanya bypass untuk verifikasi install Chrome Android
+  e.respondWith(
+    fetch(e.request).catch(() => new Response("Offline"))
+  );
 });
